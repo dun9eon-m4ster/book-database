c2106fc35a8f59d6148fe9889a2c48801e9495f7
Егор <egor1999x@mail.ru>
2026-06-13 18:15:31 +0300

    Частично реализованы классы-помощники для поиска

--- include/heterogeneous_lookup.hpp
+++ include/heterogeneous_lookup.hpp
@@ -1,14 +1,41 @@
 #pragma once
 
+#include "book.hpp"
+#include "comparators.hpp"
+#include <functional>
 #include <string>
 #include <string_view>
 
 namespace bookdb {
 
-struct TransparentStringLess {};
+struct TransparentStringLess {
+    using is_transparent = void;
 
-struct TransparentStringEqual {};
+    bool operator()(const Book &book, const std::string &title) const { return comp::LessByTitle()(book, title); }
+    bool operator()(const std::string &title, const Book &book) const { return comp::LessByTitle()(title, book); }
 
-struct TransparentStringHash {};
+    bool operator()(const Book &book, const std::string_view &author) const {
+        return comp::LessByAuthor()(book, author);
+    }
+    bool operator()(const std::string_view &author, const Book &book) const {
+        return comp::LessByAuthor()(author, book);
+    }
+};
+
+struct TransparentStringEqual {
+    using is_transparent = void;
+
+    bool operator()(const Book &book, const std::string &title) const { return book.title == title; }
+    bool operator()(const std::string &title, const Book &book) const { return title == book.title; }
+
+    bool operator()(const Book &book, const std::string_view &author) const { return book.author == author; }
+    bool operator()(const std::string_view &author, const Book &book) { return author == book.author; }
+};
+
+struct TransparentStringHash {
+    using is_transparent = void;
+
+    //???
+};
 
 }  // namespace bookdb
