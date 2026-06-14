d28d769436d8bcd71cc480a7d908d4b66c4c78cb
Егор <egor1999x@mail.ru>
2026-06-14 22:15:29 +0300

    Обновлён интерфейс, методы добавления и доступа к элементам

--- include/book_database.hpp
+++ include/book_database.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <print>
+#include <format>
 #include <string>
 #include <string_view>
-#include <unordered_map>
+#include <unordered_set>
+#include <utility>
 #include <vector>
 
 #include "book.hpp"
@@ -17,10 +18,39 @@ class BookDatabase {
 public:
     // Type aliases
     using iterator = BookContainer::iterator;
+    using size_type = BookContainer::size_type;
 
-    using AuthorContainer = std::unordered_map<std::string, TransparentStringHash, TransparentStringEqual>;
+    using AuthorContainer = std::unordered_set<std::string, TransparentStringHash, TransparentStringEqual>;
 
     BookDatabase() = default;
+
+    void PushBack(const Book &_new_book) {
+        auto copy = _new_book;
+        PushBack(std::move(copy));
+    }
+
+    void PushBack(Book &&book) {
+        auto insert_result = authors_.insert(std::string(book.author));
+        book.author = *insert_result.first;
+        books_.push_back(std::move(book));
+    }
+
+    template <typename... Args>
+        requires std::constructible_from<Book, Args...>
+    void EmplaceBack(Args &&...args) {
+        Book book(std::forward<Args>(args)...);
+        auto insert_result = authors_.insert(std::string(book.author));
+        book.author = *insert_result.first;
+        books_.emplace_back(std::move(book));
+    }
+
+    std::span<const Book> GetBooks() const { return books_; }
+
+    std::span<Book> GetBooks() { return books_; }
+
+    const AuthorContainer &GetAuthors() { return authors_; }
+
+    const AuthorContainer &GetAuthors() const { return authors_; }
 
     void Clear() {
         books_.clear();
@@ -30,6 +60,7 @@ public:
     // Standard container interface methods
     iterator begin() { return books_.begin(); }
     iterator end() { return books_.end(); }
+    size_type size() const { return books_.size(); }
 
 private:
     BookContainer books_;
@@ -39,14 +70,11 @@ private:
 }  // namespace bookdb
 
 namespace std {
+
 template <>
 struct formatter<bookdb::BookDatabase<std::vector<bookdb::Book>>> {
     template <typename FormatContext>
     auto format(const bookdb::BookDatabase<std::vector<bookdb::Book>> &db, FormatContext &fc) const {
-        /*
-        Раскомментируйте, когда bookdb::BookDatabase поддержит интерфейсы, доступные стандартным контейнерам
-        (size/begin/...)
-
         format_to(fc.out(), "BookDatabase (size = {}): ", db.size());
 
         format_to(fc.out(), "Books:\n");
@@ -58,7 +86,7 @@ struct formatter<bookdb::BookDatabase<std::vector<bookdb::Book>>> {
         for (const auto &author : db.GetAuthors()) {
             format_to(fc.out(), "- {}\n", author);
         }
-        */
+
         return fc.out();
     }
 
